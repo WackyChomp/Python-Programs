42157c729a139e69f77078f6c32a7003b7aca7fa
Wilson Chan <wilsonchan54@gmail.com>
2020-10-12 00:09:11 -0400

    Create the paddle on the left screen

--- Pong.py
+++ Pong.py
@@ -9,3 +9,23 @@ windows.setup(width = 1000 , height = 600)
 windows.tracer(0)       #prevents the window from updating and speeds up gameplay
 
 
+# Paddle 1
+paddle1 = turtle.Turtle()
+paddle1.speed(0)
+paddle1.shape("square")
+paddle1.color("white")
+paddle1.shapesize(stretch_wid = 5 , stretch_len = 1)#default shape is 20px by 20px
+paddle1.penup()         #draw lines when moving
+paddle1.goto(-350, 0)
+
+
+# Paddle 2
+
+
+# Ball
+
+
+#Main game
+while True:
+    windows.update()      #everytime the loop runs, it updates the screen
+
