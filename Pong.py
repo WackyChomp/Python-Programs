71caa2282d4da26163dde5d681398a88539f89f3
Wilson Chan <wilsonchan54@gmail.com>
2020-10-12 00:20:06 -0400

    Create ball at the center of the screen

--- Pong.py
+++ Pong.py
@@ -30,6 +30,12 @@ paddle2.goto(350, 0)       #right side
 
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)          #center of screen
 
 
 #Main game
