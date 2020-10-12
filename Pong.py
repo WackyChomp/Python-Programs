9eab1613bb52a914198203040b35f3f81bf3d183
Wilson Chan <wilsonchan54@gmail.com>
2020-10-11 23:56:18 -0400

    Create the window for the Pong game

---
+++ Pong.py
@@ -0,0 +1,11 @@
+
+import turtle         #basic graphics for games
+
+#window graphic for pong
+windows = turtle.Screen()
+windows.title("The Pong Game")
+windows.bgcolor("black")
+windows.setup(width = 1000 , height = 600)
+windows.tracer(0)       #prevents the window from updating and speeds up gameplay
+
+
