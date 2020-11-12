1e85f4fba44062e0977185e0ee144d2cb3959d82
Wilson Chan <wilsonchan54@gmail.com>
2020-11-11 19:18:57 -0500

    Loading the image assets: bird, pipe, base, and background

--- PC2 - Flappy Bird AI/Flappy_Bird.py
+++ PC2 - Flappy Bird AI/Flappy_Bird.py
@@ -8,8 +8,20 @@ import random
 WIN_WIDTH = 600
 WIN_HEIGHT = 800
 
-#Loading image of Flappy Bird
-birdImage = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), 
+#----------------------------------------------------#
+
+#Loading image of Flappy Bird / pipe / base / background
+#using snake_case
+bird_image = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), 
             pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
             pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
+
+pipe_image = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
+
+base_image = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
+
+background_image = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+
+#----------------------------------------------------#
+
 
