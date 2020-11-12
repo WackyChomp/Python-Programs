f85db08aec37dd1b6cdd08352480633466dd999c
Wilson Chan <wilsonchan54@gmail.com>
2020-11-11 19:40:49 -0500

    Creating bird class along with moving functions

--- PC2 - Flappy Bird AI/Flappy_Bird.py
+++ PC2 - Flappy Bird AI/Flappy_Bird.py
@@ -12,7 +12,7 @@ WIN_HEIGHT = 800
 
 #Loading image of Flappy Bird / pipe / base / background
 #using snake_case
-bird_image = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), 
+bird_images = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird1.png"))), 
             pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird2.png"))),
             pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bird3.png")))]
 
@@ -24,4 +24,30 @@ background_image = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs
 
 #----------------------------------------------------#
 
+class Bird:
+    images = bird_images
+    MAX_ROTATION = 25     #how much the image tilts
+    ROT_VEL = 20          #rotation velocity
+    ANIMATION_TIME = 5
 
+    def __init__(self, x, y):     #starting point of Flappy Bird
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.vel = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.images[0]      #references images > bird_images
+
+    def jump(self):
+        self.vel = 10.5        #negative velocity to move up
+        self.tick_count = 0    #keeps track of last jump
+        self.height = self.y
+
+    def move(self):
+
+while True:
+    bird.move()            #no need to calculate how much the bird moves
+
+#----------------------------------------------------#
